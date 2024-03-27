6dca2d875927db96a43028203089689240ea5bbc
Angga rizki <radmvarc@gmail.com>
2024-03-27 11:54:47 +0700

    12 Valid Braces

--- 11#productOfConsecutiveFibNumbers.js
+++ 11#productOfConsecutiveFibNumbers.js
@@ -1,13 +1,13 @@
-const productFib = prod => {
+const productFib = (prod) => {
   // Inisialisasi dua variabel a dan b dengan nilai awal 0 dan 1
   let [a, b] = [0, 1];
-  
+
   // Loop while untuk mencari dua bilangan Fibonacci yang memiliki produk lebih dari atau sama dengan prod
   while (a * b < prod) {
     // Perbarui nilai a dengan b, dan b dengan a + b (aturan bilangan Fibonacci)
     [a, b] = [b, a + b];
   }
-  
+
   // Kembalikan array yang berisi a, b, dan status kebenaran apakah a * b sama dengan prod atau tidak
   return [a, b, a * b === prod];
 };
